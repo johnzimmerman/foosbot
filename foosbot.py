328118598ee9628a8ac7e4ba7f9d6706cc3abe1c
John Zimmerman <jfzimmerman@gmail.com>
2011-12-20 17:46:33 -0600

    Updating foosbot, but its currently broken.

--- foosbot.py
+++ foosbot.py
@@ -1,12 +1,18 @@
 from ConfigParser import SafeConfigParser
 import logging
+import random
 
 from sleekxmpp import ClientXMPP
 from sleekxmpp.exceptions import IqError, IqTimeout
 
+
 # Load the config file
 cfgparser = SafeConfigParser()
 cfgparser.read('foosbot.cfg')
+
+# Game configuration
+game_requested = False
+active_players = []
 
 class FoosBot(ClientXMPP):
 
@@ -35,19 +41,45 @@ class FoosBot(ClientXMPP):
             self.disconnect()
 
     def message(self, msg):
-        if msg['type'] in ('chat', 'normal'):
-            msg.reply("Thanks for sending\n%(body)s" % msg).send()
-
+        if msg['type'] not in ('chat', 'normal'):
+            # TODO
+            # Add logging
+            return
+            
+        sender = str(msg["from"]).split("/")[0]
+        
+        # Check if the person requesting the game is on the players list
+        if not cfgparser.has_option('Players', sender):
+            msg.reply('I\'m sorry, but I don\'t recognize you.').send()
+            return
+            
+        player = cfgparser.get('Players', sender)
+        body = str(msg["body"]).strip().lower()
+        
+        if body == 'play' and game_requested == False:
+            # Set game state to requested
+            game_requested = True
+            # Add requesting player to active players list
+            active_players.append(player)
+            # Send message to other registered players
+            registered_players = cfgparser.options('Players')
+            for rp in registered_players:
+                self.send_message(mto=rp,
+                                  mbody='%s has challenged you to a game of f00sball!' % player,
+                                  mtype='chat')
+            #msg.reply('Oh Hai, %s' % player).send()
+            
+         
+        #msg.reply("Thanks for sending me a message %(from)s\n%(body)s" % msg).send()
+    
 
 if __name__ == '__main__':
-    # Ideally use optparse or argparse to get JID,
-    # password, and log level.
-
     logging.basicConfig(level=logging.DEBUG,
                         format='%(levelname)-8s %(message)s')
                         
     jid = cfgparser.get('Connection', 'jid')
     password = cfgparser.get('Connection', 'password')
+    
     xmpp = FoosBot(jid, password)
     xmpp.connect(('talk.google.com', 5222))
     xmpp.process(block=True)
